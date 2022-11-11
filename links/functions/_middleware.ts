582fc76e088f43f8fc6203cd88802cf224fcff17
MACH2Simulations <9164503+MACH2Simulations@users.noreply.github.com>
2022-11-11 23:14:50 +0000

    Update _middleware.ts

--- links/functions/_middleware.ts
+++ links/functions/_middleware.ts
@@ -1,53 +1,15 @@
-import { create } from "twind";
-import type { TW } from "twind";
-import { virtualSheet, getStyleTag } from "twind/sheets";
-import type { PluginArgs } from "..";
+import twindPlugin from "pages-plugin-twind";
 
-const sheet = virtualSheet();
+const hello = async ({ next }) => {
+  const response = await next();
+  response.headers.set("X-Hello", "Hello from functions Middleware!");
+  return response;
+};
 
-export const onRequest: PagesPluginFunction<
-  unknown,
-  any,
-  Record<string, unknown>,
-  PluginArgs
->[] = [
-  async ({ next, pluginArgs = {} }) => {
-    console.log("Hey");
-    const response = await next();
-    // if (response.headers.get("content-type") !== "text/html") return response;
-    sheet.reset();
-    const { tw } = create({
-      ...pluginArgs,
-      sheet,
-      mode: "silent",
-    });
-    await new HTMLRewriter()
-      .on("*", new StyleExtractor(tw))
-      .transform(response.clone())
-      .text();
-    return new HTMLRewriter()
-      .on("head", new StyleInjector(getStyleTag(sheet)))
-      .transform(response);
-  },
+
+export const onRequest: PagesFunction[] = [
+  twindPlugin({
+    darkMode: "class"
+  }),
+  hello
 ];
-
-class StyleExtractor implements HTMLRewriterElementContentHandlers {
-  tw: TW;
-  constructor(tw: TW) {
-    this.tw = tw;
-  }
-  element(e: Element) {
-    const classes = Object.fromEntries(e.attributes).class;
-    this.tw(classes);
-  }
-}
-
-class StyleInjector implements HTMLRewriterElementContentHandlers {
-  styleTag: string;
-  constructor(styleTag: string) {
-    this.styleTag = styleTag;
-  }
-  element(head: Element) {
-    head.append(this.styleTag, { html: true });
-  }
-}
